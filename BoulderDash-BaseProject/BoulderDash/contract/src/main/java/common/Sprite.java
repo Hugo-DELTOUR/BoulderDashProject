1556c904cf9dbbe0d5603da98259a1dfeafa288e
zelra <pierre.gilly@viacesi.fr>
2017-06-22 10:01:56 +0200

    model currently working

--- BoulderDash-BaseProject/BoulderDash/contract/src/main/java/common/Sprite.java
+++ BoulderDash-BaseProject/BoulderDash/contract/src/main/java/common/Sprite.java
@@ -1,6 +1,7 @@
 package common;
 
 import java.awt.Image;
+import java.awt.image.BufferedImage;
 
 public class Sprite {
 	
@@ -13,7 +14,7 @@ public class Sprite {
 		
 	}
 	
-	public Sprite(char character, String imageName){
+	public Sprite(char character, BufferedImage bufferedImage){
 		
 	}
 
--- BoulderDash-BaseProject/BoulderDash/model/src/main/java/model/element/motionlessElement/Air.java
+++ BoulderDash-BaseProject/BoulderDash/model/src/main/java/model/element/motionlessElement/Air.java
@@ -1,11 +1,24 @@
 package model.element.motionlessElement;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
 import common.Permeability;
 import common.Sprite;
 
 public class Air extends MotionlessElement {
 
-	public static final Sprite sprite = new Sprite(' ', /*TODO*/);
+	static BufferedImage bigImg = ImageIO.read(new File("ress/74359.png"));
+	
+	final static int width = 16;
+	final static int height = 16;
+	final static int rows = 5;
+	final static int cols = 5;
+	
+	public static final Sprite sprite = new Sprite(' ', bigImg.getSubimage(cols * width, rows *height ,width,height));
+
 	
 	public Air(Sprite sprite, Permeability permeability) {
 		super(sprite, permeability);
